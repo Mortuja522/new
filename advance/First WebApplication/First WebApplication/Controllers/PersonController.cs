f0e0acb1ac6d45a70836850e25afb9fe4dcdb9be
Mortuja Morshed <75001023+Mortuja522@users.noreply.github.com>
2021-10-04 14:04:21 +0600

    new

---
+++ advance/First WebApplication/First WebApplication/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First_WebApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
